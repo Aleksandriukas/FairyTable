9678ec342376aac7b98c938df7cbe7f5e470682c
Aleksandriukas <aleksandriukas777@gmail.com>
2024-03-13 13:28:52 +0200

    Added iOS support

--- src/app/(main)/menu/page.tsx
+++ src/app/(main)/menu/page.tsx
@@ -1,53 +1,59 @@
-import { Pressable, Text, FlatList, View } from 'react-native';
-import { useEffect, useState } from 'react';
-import { DishBean } from '../../../beans/DishBean';
-import { supabase } from '../../../supabase/supabase';
-import { Appbar, useTheme } from 'react-native-paper';
-import { useLinkTo } from '../../../../charon';
-import { DishListItem } from './DishListItem';
+import { Pressable, Text, FlatList, View } from "react-native";
+import { useEffect, useState } from "react";
+import { DishBean } from "../../../beans/DishBean";
+import { supabase } from "../../../supabase/supabase";
+import { Appbar, useTheme } from "react-native-paper";
+import { useLinkTo } from "../../../../charon";
+import { DishListItem } from "./DishListItem";
 
 export default function MainPage() {
-    const linkTo = useLinkTo();
+  const linkTo = useLinkTo();
 
-    const [dishes, setDishes] = useState<DishBean[] | undefined>(undefined);
+  const [dishes, setDishes] = useState<DishBean[] | undefined>(undefined);
 
-    const { colors } = useTheme();
+  const { colors } = useTheme();
 
-    const fetchDishes = async () => {
-        try {
-            const { data, error } = await supabase.from('Dish').select('*');
-            if (error) {
-                throw error;
-            }
+  const fetchDishes = async () => {
+    try {
+      const { data, error } = await supabase.from("Dish").select("*");
+      if (error) {
+        throw error;
+      }
 
-            setDishes(data);
-        } catch (e) {
-            console.log(e);
-        }
-    };
-    useEffect(() => {
-        fetchDishes();
-    }, []);
+      setDishes(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    fetchDishes();
+  }, []);
 
-    return (
-        <View>
-            <Appbar.Header style={{ backgroundColor: colors.primaryContainer }}>
-                <Appbar.Content title="Menu" />
-                <Appbar.Action
-                    onPress={() => {
-                        linkTo('/auth');
-                    }}
-                    icon="account"
-                />
-            </Appbar.Header>
-            <FlatList
-                ItemSeparatorComponent={() => {
-                    return <View style={{ height: 1, backgroundColor: colors.outlineVariant }} />;
-                }}
-                renderItem={({ item, index }) => <DishListItem data={item} key={index} />}
-                keyExtractor={(item) => item.id.toString()}
-                data={dishes}
+  return (
+    <View style={{ flex: 1 }}>
+      <Appbar.Header style={{ backgroundColor: colors.primaryContainer }}>
+        <Appbar.Content title="Menu" />
+        <Appbar.Action
+          onPress={() => {
+            linkTo("/auth");
+          }}
+          icon="account"
+        />
+      </Appbar.Header>
+      <FlatList
+        ItemSeparatorComponent={() => {
+          return (
+            <View
+              style={{ height: 1, backgroundColor: colors.outlineVariant }}
             />
-        </View>
-    );
+          );
+        }}
+        renderItem={({ item, index }) => (
+          <DishListItem data={item} key={index} />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        data={dishes}
+      />
+    </View>
+  );
 }
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,26 +1,28 @@
-import { useLinkTo, useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
-import { Pressable, Text } from 'react-native';
-import { Button } from 'react-native-paper';
-
+import { useLinkTo, useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import { Pressable, Text } from "react-native";
+import { Button } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function RegisterPage() {
-    const { goBack } = useNavigation();
-    const linkTo = useLinkTo();
-    const [isAdmin, setIsAdmin] = useState(true);
-    return (
-        <Pressable
-            onPress={() => {
-                goBack();
-            }}
+  const { goBack } = useNavigation();
+  const linkTo = useLinkTo();
+  const [isAdmin, setIsAdmin] = useState(true);
+  return (
+    <SafeAreaView>
+      <Pressable
+        onPress={() => {
+          goBack();
+        }}
+      >
+        <Text>auth</Text>
+        <Button
+          onPress={() => {
+            linkTo(`/auth/${isAdmin ? "admin" : "sef"}`);
+          }}
         >
-            <Text>auth</Text>
-            <Button
-                onPress={() => {
-                    linkTo(`/auth/${isAdmin ? 'admin' : 'sef'}`);
-                }}
-            >
-                Sign in
-            </Button>
-        </Pressable>
-    );
+          Sign in
+        </Button>
+      </Pressable>
+    </SafeAreaView>
+  );
 }
