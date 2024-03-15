34e23a27820ee0d7a612d076095fa2434298b4c1
Nadezda Pyzevskaja <nadezda.pyzevskaja@stud.vilniustech.lt>
2024-03-15 18:22:58 +0200

    Добавлены цвета красные

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,35 @@
 import { PropsWithChildren, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { useTheme } from "react-native-paper";
+import { PaperProvider, useTheme } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import {
+  MD3LightTheme as DefaultTheme,
+} from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  // Specify custom property in nested object
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#A00000',
+    primaryContainer: '#A00000A0',
+  },
+};
 
 export default function MainLayout({ children }: PropsWithChildren<{}>) {
   const { colors } = useTheme();
   return (
-    <SafeAreaProvider>
-      <View
-        style={[StyleSheet.absoluteFill, { backgroundColor: colors.surface }]}
-      >
-        {children}
-      </View>
-    </SafeAreaProvider>
+    <PaperProvider theme={theme}>
+
+
+      <SafeAreaProvider>
+        <View
+          style={[StyleSheet.absoluteFill, { backgroundColor: colors.surface }]}
+          >
+          {children}
+        </View>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 }
