81a96aef6f2a568188e240ab8a9ffc53fcdfacd0
pauliusmpetrauskas <pauliusm.petrauskas@gmail.com>
2024-06-02 18:02:42 +0300

    error

---
+++ src/app/layout.tsx
@@ -0,0 +1,54 @@
+import { PropsWithChildren, useEffect, useId } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { PaperProvider, useTheme } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import uuid from "react-native-uuid";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { MD3LightTheme as DefaultTheme } from "react-native-paper";
+import { supabase } from "../supabase/supabase";
+
+const pink = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primaryContainer: "#ECB1C4",
+    primary: "#B51A5E",
+    surface: "#FAEEEF",
+    background: "#FAEEEF",
+    secondaryContainer: "#ffdde8",
+  },
+};
+
+export default function MainLayout({ children }: PropsWithChildren<{}>) {
+  useEffect(() => {
+    const setId = async () => {
+      try {
+        const id = await AsyncStorage.getItem("uuid");
+
+        console.log(id);
+
+        if (!id) {
+          await AsyncStorage.setItem("uuid", uuid.v4().toString());
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    setId();
+  }, []);
+  supabase.auth.signOut();
+
+  const { colors } = useTheme();
+  return (
+    <PaperProvider theme={pink}>
+      <SafeAreaProvider>
+        <View
+          style={[StyleSheet.absoluteFill, { backgroundColor: colors.surface }]}
+        >
+          {children}
+        </View>
+      </SafeAreaProvider>
+    </PaperProvider>
+  );
+}
